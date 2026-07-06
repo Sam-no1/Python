9b945377f4397d2d3792e6f147397b62b80769b7
Sam <154284287+Sam-no1@users.noreply.github.com>
2026-07-06 10:28:57 -0700

    Add files via upload

---
+++ zeros73.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        # transpose first
+        for i in range(len(matrix)):
+            for j in range(i+1, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # reverse
+        for i in range(len(matrix)):
+            for j in range(len(matrix)//2):
+                matrix[i][j], matrix[i][len(matrix)-j-1] = matrix[i][len(matrix)-j-1], matrix[i][j]
+                
